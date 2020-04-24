7e0259ae5797aa29b49da98a4f0caffe80e1591b
shaniyang <63471552+shaniyang@users.noreply.github.com>
2020-04-24 18:47:48 +0300

    7.4 valmis

---
+++ Viikko74/app/src/main/java/com/example/viikko74/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.viikko74;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView text = findViewById(R.id.text); //alustus
+        final EditText edit = findViewById(R.id.editText);
+
+        edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String newText = edit.getText().toString(); //ottaa tekstin ja muuttaa string muotoon
+                text.setText(newText);
+            }
+        });
+
+
+    }
+}
