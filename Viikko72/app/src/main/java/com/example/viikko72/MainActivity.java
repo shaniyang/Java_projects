90cf5192f35b68e2ba10e351894d830d8686eaa0
shaniyang <63471552+shaniyang@users.noreply.github.com>
2020-04-24 18:14:15 +0300

    7.2 valmis

--- Viikko72/app/src/main/java/com/example/viikko72/MainActivity.java
+++ Viikko72/app/src/main/java/com/example/viikko72/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.viikko72;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button); //Button luokka ja sijainti
+        final TextView text = findViewById(R.id.text);
+
+        text.setText("Tämä on testi teksti."); //text. kutsuu
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setText("Hello World!");
+            }
+        });
     }
 }
+
