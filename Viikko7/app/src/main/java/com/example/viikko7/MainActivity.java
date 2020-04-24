5eb114462d81522069dbb5935b36dcafcf9c22a5
shaniyang <63471552+shaniyang@users.noreply.github.com>
2020-04-24 17:56:43 +0300

    Initial commit

---
+++ Viikko7/app/src/main/java/com/example/viikko7/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.viikko7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                System.out.println("Hello World!");
+            }
+        });
+    }
+}
