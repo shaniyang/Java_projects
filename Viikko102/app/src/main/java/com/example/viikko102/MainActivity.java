a724acc04dcfb3396e6b20a21b9633b157f1930f
shaniyang <63471552+shaniyang@users.noreply.github.com>
2020-04-25 21:58:25 +0300

    10.2 valmis

---
+++ Viikko102/app/src/main/java/com/example/viikko102/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.viikko102;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String url_add;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button go = findViewById(R.id.searchbutton);
+        Button refresh = findViewById(R.id.refreshbutton);
+        final EditText text = findViewById(R.id.text);
+        final WebView web = findViewById(R.id.webButton);
+        web.setWebViewClient(new WebViewClient());
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadUrl("https://lut.fi");
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                web.setWebViewClient(new WebViewClient());
+                url_add = text.getText().toString();
+                if (!url_add.startsWith("http://") && !url_add.startsWith("https://")){
+                    url_add = "http://" + url_add;
+                }
+                web.loadUrl(url_add);
+
+            }
+        });
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                url_add = web.getUrl();
+                web.loadUrl(url_add);
+            }
+        });
+    }
+}
